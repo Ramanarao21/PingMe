084e766900d79425f7763b3cb444adae00fea42d
Ramana <140582569+Ramanarao21@users.noreply.github.com>
2025-04-04 10:18:39 +0530

    Update SettingsPage.jsx

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -13,8 +13,9 @@ const SettingsPage = () => {
 
   const {theme, setTheme} = useThemeStore();
   return (
-    <div className='h-screen container mx-auto px-4 pt-20 max-w-5xl'>
-      <div className='space-y-6'>
+    <div className='min-h-screen w-full bg-base-100"'>
+       <div className='container mx-auto px-4 pt-20 max-w-5xl'> 
+                  <div className='space-y-6'>
         <div className='flex flex-col gap-1'>
           <h2 className='text-lg font-semibold '> Theme</h2>
           <p className='text-sm text-base-container/70'> Choose a theme for your chat interface </p>
@@ -113,7 +114,7 @@ const SettingsPage = () => {
         </div>
 
       </div>
-
+       </div>
     </div>
   );
 };
